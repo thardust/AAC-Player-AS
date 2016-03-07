7f8236fd17ff562ea7b7977bcf9f1c2d43e14c4a
Thardust <lcarranza.95@gmail.com>
2016-03-06 23:20:40 -0500

    AAC Player AS
    Adaptación del AAC Player a Android Studio.

---
+++ app/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/app/C_/AndroidApps/aacplayer-android-r25/app/src/main/jni/aac-decoder/aac-array-common.o.d
@@ -0,0 +1,32 @@
+C:\AndroidApps\aacplayer-android-r25\app\build\intermediates\ndk\debug\obj/local/arm64-v8a/objs/app/C_\AndroidApps\aacplayer-android-r25\app\src\main\jni\aac-decoder\aac-array-common.o: \
+ C:\AndroidApps\aacplayer-android-r25\app\src\main\jni\aac-decoder\aac-array-common.c \
+ C:\AndroidApps\aacplayer-android-r25\app\src\main\jni\aac-decoder\aac-array-common.h \
+ C:\AndroidApps\aacplayer-android-r25\app\src\main\jni\aac-decoder\aac-common.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/log.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/string.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/malloc.h \
+ C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/xlocale.h
+
+C:\AndroidApps\aacplayer-android-r25\app\src\main\jni\aac-decoder\aac-array-common.h:
+
+C:\AndroidApps\aacplayer-android-r25\app\src\main\jni\aac-decoder\aac-common.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/jni.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/android/log.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/string.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/malloc.h:
+
+C:/Android/sdk/ndk-bundle/platforms/android-21/arch-arm64/usr/include/xlocale.h:
